676b885aa16a401d21d53bf6ff9144b9d7f3e088
Alexander Walther | ObsidianDestroyer <asushofman@gmail.com>
2022-08-04 18:46:26 +0300

    [MASTER] Added complete variant of wc with functionality to count bytes

--- gwc/main.go
+++ gwc/main.go
@@ -5,22 +5,26 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/urfave/cli/v2"
 )
 
+var helpUsage = "Prints the number of characters of each line, words and bytes for each FILE and rmultiple files. " +
+	"According to a sequence of non-zero sampled characters, separated by whitespace."
+
 func main() {
 	applicationFlags := []cli.Flag{
 		&cli.BoolFlag{
-			Name: "c",
-			Usage: "Print count of bytes",
+			Name:    "c",
+			Usage:   "Print count of bytes",
 			Aliases: []string{"bytes"},
 		},
 	}
 
 	app := &cli.App{
-		Name:  "gwc",
-		Usage: "Prints the number of characters of each line, words and bytes for each FILE and rmultiple files. According to a sequence of non-zero sampled characters, separated by whitespace.",
+		Name:   "gwc",
+		Usage:  helpUsage,
 		Flags:  applicationFlags,
 		Action: execute,
 	}
@@ -28,28 +32,34 @@ func main() {
 	check(err)
 }
 
-func execute(cli *cli.Context) error {
-	filePath := cli.Args().Get(0)
-	if filePath == "" {
-		fmt.Println("Please, type a path to file.")
-		os.Exit(3)
-	} else {
-		byteStream := readFile(filePath)
-		count := countBytes(byteStream)
-		fmt.Print(count)
-	}
-	mustCountBytes := cli.Bool("c")
-	if mustCountBytes == true {
-
-	}
-	return nil
-}
-
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 		cli.Exit("Encountered unhandled error", 1)
 	}
+}
+
+func execute(cli *cli.Context) error {
+	var completeString string
+	var count int
+
+	filePath := cli.Args().Get(0)
+	fileName := filepath.Base(filePath)
+
+	if filePath == "" {
+		fmt.Println("Please, type a path to file.")
+		os.Exit(3)
+	}
+
+	byteStream := readFile(filePath)
+
+	if cli.Bool("c") == true {
+		count = countBytes(byteStream)
+		completeString += fmt.Sprint(count)
+	}
+
+	fmt.Println(completeString, fileName)
+	return nil
 }
 
 func readFile(filePath string) []byte {
